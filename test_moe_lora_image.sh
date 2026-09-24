0bec99537cc0f61b2a9e1931363a672556beae11
cx-333 <1354123040@qq.com>
2026-09-24 11:36:14 +0800

    initial

--- test_moe_lora_image.sh
+++ test_moe_lora_image.sh
@@ -2,6 +2,8 @@
 
 set -e 
 
+# mode: fidelity or perception 
+
 python -m playground.test_moe_lora_image_model \
     --data_path "./assets" \
     --quant_level 15 \
@@ -9,6 +11,6 @@ python -m playground.test_moe_lora_image_model \
     --mode fidelity \
     --save_dir ./results/fidelity \
     --save \
-    --model_path "F:/research-code/2026/pretrained/aligned_cvpr2025_image.pth.tar" \
+    --model_path "ckpts/pretrained_mse_model.pth.tar" \
     --lora_path "ckpts/latest_moe_lora_checkpoint_qp0_63_qcond.pth.tar"
 
